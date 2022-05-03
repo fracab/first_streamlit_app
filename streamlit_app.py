546fbd124d892bf19d139c20b679a2768de8d179
Francesco Caberlin <f.caberlin@gmail.com>
2022-05-03 14:45:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit
 streamlit.title('My parents new healthy diner')
 streamlit.header('This is an header')
 streamlit.text('This is text \n new line')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
