850419349d513b03d5ee026d2d5e627b8bdf671c
Francesco Caberlin <f.caberlin@gmail.com>
2022-05-03 14:43:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My parents new healthy diner')
 streamlit.header('This is an header')
-streamlit.text('This is text /n new line')
+streamlit.text('This is text \n new line')
